b52f73871266591588fd275fcdb6bc12ca05a1a1
KFC <hankfchan@gmail.com>
2020-07-17 09:56:36 +0800

    yarn lib

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 // vendor
 // import { Button } from "@salesforce/design-system-react";
+import "@salesforce-ux/design-system";
+import "animate.css";
 import "./styles/App.scss";
 
 function App() {
