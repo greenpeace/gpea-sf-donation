104e63c50297797185c0dd664984f5fa250881c9
KFC <hankfchan@gmail.com>
2020-07-20 16:15:34 +0800

    start developing extra blocks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from "react";
 // vendor
 // import { Button } from "@salesforce/design-system-react";
-import "@salesforce-ux/design-system";
+import "@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.min.css";
 import "animate.css";
+// import Swiper JS
+import Swiper from "swiper";
+import "swiper/swiper-bundle.css";
+//
 import "./styles/App.scss";
+//
+const swiperParams = {
+  loop: true,
+  slidesPerView: "auto",
+  centeredSlides: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+};
+const swiper = new Swiper(".swiper-container", swiperParams);
 
 function App() {
   return <div className="app" style={{ display: "none" }}></div>;
