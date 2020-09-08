f717d4c82c265d1649c6338ef2db00aa4881e494
KFC <hankfchan@gmail.com>
2020-09-08 15:48:36 +0800

    re organized css structure

--- src/App.js
+++ src/App.js
@@ -1,43 +1,49 @@
 import React from "react";
 // import { Button } from "@salesforce/design-system-react";
-//
 import "@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.min.css";
-import "swiper/swiper-bundle.css";
-import Swiper from "swiper";
-// import "aos/dist/aos.css";
-// import AOS from "aos/";
+import { Swiper, Navigation, Pagination } from "swiper";
+import parallax from "./scripts/parallax";
 //
 import "./styles/App.scss";
 //
+Swiper.use([Navigation, Pagination]);
+//
 const swiperParams = {
   loop: true,
-  loopedSlides: 1,
   slidesPerView: "auto",
   centeredSlides: true,
-  spaceBetween: 20,
   pagination: {
     el: ".swiper-pagination",
     type: "bullets",
     dynamicBullets: true,
+    clickable: true,
   },
-  breakpoints: {
-    991: {
-      spaceBetween: 40,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    clickable: true,
+  },
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  // eslint-disable-next-line no-unused-vars
+  const swiper = new Swiper(".swiper-container", swiperParams);
+  const swiperMixedContent = new Swiper(
+    ".section-mixed-content .swiper-container--mixed",
+    {
+      slidesPerView: 1,
+      simulateTouch: false,
+      autoplay: true,
+      pagination: {
+        el: ".swiper-pagination",
+        type: "bullets",
         clickable: true,
       },
-    },
-  },
-};
-const swiper = new Swiper(".swiper-container", swiperParams);
-const aosParams = {
-  easing: "ease-in-out-cubic",
-  once: true,
-  disable: "phone",
-};
-// AOS.init(aosParams);
+    }
+  );
+  parallax();
+});
+
 function App() {
   return <div className="app" style={{ display: "none" }}></div>;
 }
