949c7ff2b888fbdc001037353d5b6b67959415de
KFC <chris.chan@greenpeace.org>
2021-03-16 15:51:42 +0800

    Revert "prettier"
    This reverts commit 2c0da151fb775c1e1e95615ba42cc536ff3100ac.

--- src/assets/static/GPEADonateResources/js/public-donation.js
+++ src/assets/static/GPEADonateResources/js/public-donation.js
@@ -1,10 +1,6 @@
 $(function () {
   //scroll
-  if (
-    "IntersectionObserver" in window &&
-    "IntersectionObserverEntry" in window &&
-    "intersectionRatio" in window.IntersectionObserverEntry.prototype
-  ) {
+  if ("IntersectionObserver" in window && "IntersectionObserverEntry" in window && "intersectionRatio" in window.IntersectionObserverEntry.prototype) {
     var observer = new IntersectionObserver(function (entries) {
       if (entries[0].boundingClientRect.y < 0) {
         document.body.classList.add("bottom-fix");
@@ -16,24 +12,21 @@ $(function () {
   }
 
   //pararell
-  var image = document.getElementsByClassName("topimg-move");
+  var image = document.getElementsByClassName('topimg-move');
   new simpleParallax(image, {
-    delay: 0.6,
-    transition: "cubic-bezier(0,0,0,1)",
+    delay: .6,
+    transition: 'cubic-bezier(0,0,0,1)'
   });
 
-  $(".form__close").click(function () {
+  $( ".form__close" ).click(function() {  
     $(".main__form-moblie-wrapper").css("bottom", "-1000px");
-    $("body").removeClass("fullscreen-form");
+      $("body").removeClass('fullscreen-form');
   });
 
   //move to form
   $(".header__donate-btn a").on("click", function (e) {
     e.preventDefault();
-    $("html,body").animate(
-      { scrollTop: $(".main__top-img").offset().top },
-      "slow"
-    );
+    $('html,body').animate({scrollTop: $(".main__top-img").offset().top},'slow');
   });
 
   //form collaped
@@ -41,18 +34,16 @@ $(function () {
 
   for (i = 0; i < btnColF.length; i++) {
     btnColF[i].addEventListener("click", function () {
-      var wrapperM = document.getElementsByClassName(
-        "main__form-moblie-wrapper"
-      )[0];
+      var wrapperM = document.getElementsByClassName("main__form-moblie-wrapper")[0];
       var wrapper = document.getElementsByClassName("main__form-wrapper")[0];
       wrapperM.style.bottom = null;
 
-      $("body").addClass("fullscreen-form");
+      $("body").addClass('fullscreen-form');
     });
   }
 
   //slide
-  $(".carousel__container").slick({
+  $('.carousel__container').slick({
     dots: true,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -60,3 +51,4 @@ $(function () {
     autoplaySpeed: 5000,
   });
 });
+
